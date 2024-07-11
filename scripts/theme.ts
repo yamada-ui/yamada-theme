70fc3a4be6a350064928f63d9d66ad8e4d510b48
108yen <kazuking.1911@gmail.com>
2024-07-11 22:46:34 +0900

    feat(scripts): get theme path from `node_modules`

--- scripts/theme.ts
+++ scripts/theme.ts
@@ -1,5 +1,39 @@
+import path from "path"
 import * as p from "@clack/prompts"
 import c from "chalk"
+import { glob } from "glob"
+
+export const themePath = [
+  "node_modules",
+  ".pnpm",
+  "@yamada-ui+theme@*",
+  "node_modules",
+  "@yamada-ui",
+  "theme",
+  "dist",
+  "components",
+  "*.js",
+]
+
+const resolveThemePaths = async (): Promise<string[] | undefined> => {
+  const paths = [path.join(...themePath), path.posix.join(...themePath)]
+
+  const triedPaths = await Promise.all(
+    paths.map(async (possiblePath) => {
+      const paths = await glob(possiblePath)
+
+      if (paths.length) return paths
+
+      return ""
+    }),
+  )
+
+  const resolvedPaths = triedPaths.find(Boolean)
+
+  if (!resolvedPaths) return
+
+  return resolvedPaths
+}
 
 const main = async () => {
   p.intro(c.magenta(`Generating Yamada UI theme structure`))
@@ -9,7 +43,11 @@ const main = async () => {
   try {
     const start = process.hrtime.bigint()
 
-    //TODO: do somesing
+    //TODO: get theme structure from theme file
+    const paths = await resolveThemePaths()
+    console.log(paths)
+    //TODO: get component list and create theme structure file
+    // if dont have structure file, the component is single comopnent.
 
     const end = process.hrtime.bigint()
     const duration = (Number(end - start) / 1e9).toFixed(2)
