b1155d0eeff0dae35bfd11d76c271eae9661bb3e
108yen <kazuking.1911@gmail.com>
2024-07-15 20:50:08 +0900

    chore(scripts): remove scripts

--- scripts/utils.ts
+++
@@ -1,7 +0,0 @@
-export const convertStringToObject = (content: string): any => {
-  try {
-    return new Function(`return ${content}`)()
-  } catch (e) {
-    throw new Error(`Could not convert string to object: ${content}`)
-  }
-}
